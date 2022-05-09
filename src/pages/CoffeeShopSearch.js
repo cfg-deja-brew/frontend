b0c07bc16834a753c62226080c1f4b4e57ae1b64
domgor11 <82116080+domgor11@users.noreply.github.com>
2022-05-09 22:08:54 +0100

    coffee search text styling

--- src/pages/CoffeeShopSearch.js
+++ src/pages/CoffeeShopSearch.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png'
 import CoffeeShopMap from '../components/CoffeeShopMap';
 import { getCities } from '../api/api.js'
 import './CoffeeShopSearch.css' 
@@ -10,10 +11,11 @@ function CoffeeShopSearch({cityIndex = 0}) {
   return (
     <div className="container h-100 py-5">
       <div className="dropdown">
-        <button className="btn btn-outline-dark dropdown-toggle" type="button" id="cityDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+        <img className="logo" src={logo} alt="logo"/>
+        <button className="btn btn-outline-light dropdown-toggle" type="button" id="cityDropdown" data-bs-toggle="dropdown" aria-expanded="false">
           {selectedCity.name}
         </button>
-        <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="cityDropdown">
+        <ul className="dropdown-menu dropdown-menu-light" aria-labelledby="cityDropdown">
           {cities.map((city) => <li>
             <Link to={`/coffee-shop-search/${city.name.toLowerCase()}`} className={`dropdown-item ${selectedCity.name === city.name ? 'active' : ''}`}>
               {city.name}
@@ -21,10 +23,17 @@ function CoffeeShopSearch({cityIndex = 0}) {
           </li>)}
         </ul>
       </div>
-      <h1 className="display-4">Coffee Shops in {selectedCity.name}</h1>
+      <h1 className="display-4 text-light">Coffee Shops in {selectedCity.name}</h1>
       <CoffeeShopMap city={selectedCity} />
     </div>
   )
 }
 
 export default CoffeeShopSearch
+
+
+/*
+        <button className="btn btn-outline-dark dropdown-toggle" type="button" id="cityDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+          {selectedCity.name}
+        </button>
+*/
