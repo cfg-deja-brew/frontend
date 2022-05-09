94dee17534721e6e065ec2566ea7ccf14c7ab361
domgor11 <82116080+domgor11@users.noreply.github.com>
2022-05-09 20:23:48 +0100

    add city buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,18 @@ function Home() {
     <div className="container h-100 py-5 d-flex flex-column justify-content-center align-items-center text-center text-light">
       <h1 className="text-uppercase fw-bold main-heading">Espresso Yourself</h1>
       <h2>Search small independent cafes in East Midlands</h2>
+      <br/>
+      <br/>
+      <br/>
+      <h3>Where do you want to go today?</h3>
+      <div className="city-buttons">
+        <button className="Leicester">Leicester</button>
+        <button className="Nottingham">Nottingham</button>
+        <button className="Derby">Derby</button>
+      </div>
     </div>
+    
+
   )
 }
 
