828c42eb4cb2248146366e9051006b041f1e3824
emmavharrison <92535706+emmavharrison@users.noreply.github.com>
2022-05-10 22:50:05 +0100

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,22 @@
 import React from "react";
 import logo from '../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark shadow-5-strong">
-   <a class="navbar-brand" href="#">
-    <img src={logo} width="150" height="110" alt="logo"/>
-  </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-        <li class="nav-item active">
-            <a class="nav-link" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/coffee-shop-search/leicester">Leicester</a>
-        </li>
-        </ul>
-        <ul class="navbar-nav ms-auto px-4">
-        <li class="nav-item">
-            <a class="nav-link" href="#">Log in</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Sign Up</a>
-        </li>
-        </ul>
-    </div>
+    <nav className="navbar fixed-top navbar-expand-md navbar-dark text-light shadow-5-strong">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/"><img className="logo" src={logo} alt="logo"/></Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="navbar-nav ms-auto px-4">
+            <Link role="button" to="/users" className="nav-link ms-0 ms-md-2 pt-0 pt-md-4 text-light text-end">Log In</Link>
+            <Link role="button" to="/books" className="nav-link ms-0 ms-md-2 pt-0 pt-md-4 text-light text-end">Sign Up</Link>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
