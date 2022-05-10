c7d8deba74ec729ccfe2e812d8a6d7aa33416e59
emmavharrison <92535706+emmavharrison@users.noreply.github.com>
2022-05-10 14:18:45 +0100

    added more styling for search page

--- src/pages/CoffeeShopSearch.js
+++ src/pages/CoffeeShopSearch.js
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png'
 import CoffeeShopMap from '../components/CoffeeShopMap';
 import { getCities } from '../api/api.js'
 import './CoffeeShopSearch.css' 
@@ -10,21 +9,33 @@ function CoffeeShopSearch({cityIndex = 0}) {
 
   return (
     <div className="container h-100 py-5">
-      <div className="dropdown">
-        <img className="logo" src={logo} alt="logo"/>
-        <button className="btn btn-outline-light dropdown-toggle" type="button" id="cityDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-          {selectedCity.name}
-        </button>
-        <ul className="dropdown-menu dropdown-menu-light" aria-labelledby="cityDropdown">
-          {cities.map((city) => <li>
-            <Link to={`/coffee-shop-search/${city.name.toLowerCase()}`} className={`dropdown-item ${selectedCity.name === city.name ? 'active' : ''}`}>
-              {city.name}
-            </Link>
-          </li>)}
-        </ul>
+      <h1 className="d-flex align-items-center display-4 text-light mb-4">
+        Coffee Shops in&ensp;
+        <span className="dropdown">
+          <button className="d-inline-flex align-items-center btn btn-lg btn-outline-light dropdown-toggle" type="button" id="cityDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+            <h1 className="display-4 mb-0">{selectedCity.name}&nbsp;</h1>
+          </button>
+          <ul className="dropdown-menu dropdown-menu-light" aria-labelledby="cityDropdown">
+            {cities.map((city) => <li>
+              <Link to={`/coffee-shop-search/${city.name.toLowerCase()}`} className={`dropdown-item ${selectedCity.name === city.name ? 'active' : ''}`}>
+                {city.name}
+              </Link>
+            </li>)}
+          </ul>
+        </span>
+      </h1>
+      <div className="row h-100">
+        <div className="col-md-8">
+          <CoffeeShopMap city={selectedCity} />
+        </div>
+        <div className="col-md-4">
+          <div className="card">
+            <div className="card-body">
+              Hello World
+            </div>
+          </div>
+        </div>
       </div>
-      <h1 className="display-4 text-light">Coffee Shops in {selectedCity.name}</h1>
-      <CoffeeShopMap city={selectedCity} />
     </div>
   )
 }
@@ -32,8 +43,3 @@ function CoffeeShopSearch({cityIndex = 0}) {
 export default CoffeeShopSearch
 
 
-/*
-        <button className="btn btn-outline-dark dropdown-toggle" type="button" id="cityDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-          {selectedCity.name}
-        </button>
-*/
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png'
 import './Home.css';
 
 function Home() {
   return (
     <div className="container h-100 py-5 d-flex flex-column justify-content-center align-items-center text-center text-light">
-      <img className="logo" src={logo} alt="logo"/>
       <h1 className="text-uppercase fw-bold main-heading">Espresso Yourself</h1>
       <h2 className="pb-5">Search small independent cafes in East Midlands</h2>
       <h3>Where do you want to go today?</h3>
