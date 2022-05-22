848ccf22fa72bd1b562db1b3825c4057d88cf13e
emmavharrison <92535706+emmavharrison@users.noreply.github.com>
2022-05-22 19:42:54 +0100

    added like and dislike button, not fully working yet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from "@lyket/react";
 import Home from './pages/Home';
 import CafeSearch from './pages/CafeSearch';
 import Login from './pages/Login';
@@ -13,16 +14,23 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/cafe-search/leicester" element={<CafeSearch cityIndex={0} />} />
-        <Route path="/cafe-search/nottingham" element={<CafeSearch cityIndex={1} />} />
-        <Route path="/cafe-search/derby" element={<CafeSearch cityIndex={2} />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider apiKey="pt_6c8d751a2c1d4fd85ce3ab63caccdf">
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/cafe-search/leicester" element={<CafeSearch cityIndex={0} />} />
+          <Route path="/cafe-search/nottingham" element={<CafeSearch cityIndex={1} />} />
+          <Route path="/cafe-search/derby" element={<CafeSearch cityIndex={2} />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
+
+
+
+
+
